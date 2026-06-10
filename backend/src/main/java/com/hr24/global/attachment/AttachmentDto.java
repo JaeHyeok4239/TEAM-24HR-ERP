f4d7be0d9405adbb4b41cccc6c3730eddf84e390
hansoobin-11 <vcoart@naver.com>
2026-06-10 18:15:51 +0900

    feat: 공용 첨부 파일 서비스 구현

---
+++ backend/src/main/java/com/hr24/global/attachment/AttachmentDto.java
@@ -0,0 +1,26 @@
+package com.hr24.global.attachment;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AttachmentDto {
+	private Long   attachmentId;
+    private String originalName;
+    private String storedName;
+    private String attachmentType;
+    private Long   attachmentSize;
+    private Long   uploader;
+
+    public static AttachmentDto from(Attachment attachment) {
+        return AttachmentDto.builder()
+                .attachmentId(attachment.getAttachmentId())
+                .originalName(attachment.getOriginalName())
+                .storedName(attachment.getStoredName())
+                .attachmentType(attachment.getAttachmentType())
+                .attachmentSize(attachment.getAttachmentSize())
+                .uploader(attachment.getUploader().getEmployeeId())
+                .build();
+    }
+}
---
+++ backend/src/main/java/com/hr24/global/attachment/AttachmentRepository.java
@@ -0,0 +1,7 @@
+package com.hr24.global.attachment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+
+}
