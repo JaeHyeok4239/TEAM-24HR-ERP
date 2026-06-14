c77e47b30a3ed432c41de764cb66283d83b20591
hansoobin-11 <vcoart@naver.com>
2026-06-15 01:06:54 +0900

    feat : 메뉴 디자인 변경(하위 메뉴 추가) 및 헤더 js 생성

---
+++ frontend/src/components/approval/MyDocument.js
@@ -0,0 +1,12 @@
+import Header from "../Header";
+
+export default function MyDocument() {
+  return (
+    <>
+      <Header title="내 문서함" />
+      <div className="bg-white h-screen">
+        Content
+      </div>
+    </>
+  );
+}
