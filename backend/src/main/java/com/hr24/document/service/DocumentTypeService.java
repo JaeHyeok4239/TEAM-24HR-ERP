78aac99e52eb18fcaa87b02974fabcb69512b50e
hansoobin-11 <vcoart@naver.com>
2026-06-15 18:02:49 +0900

    feat : 관리자 - 문서 유형 생성 기능 추가

--- backend/src/main/java/com/hr24/document/service/DocumentTypeService.java
+++ backend/src/main/java/com/hr24/document/service/DocumentTypeService.java
@@ -2,12 +2,23 @@ package com.hr24.document.service;
 
 import org.springframework.stereotype.Service;
 
-import lombok.RequiredArgsConstructor;
+import com.hr24.document.entity.DocumentType;
+import com.hr24.document.repository.DocumentTypeRepository;
 
+import lombok.RequiredArgsConstructor;
 
 
 @Service
 @RequiredArgsConstructor
 public class DocumentTypeService{
 	
+	private final DocumentTypeRepository documentTypeRepository;
+	
+	public DocumentType createType(DocumentType documentType) {
+		
+		DocumentType saved = documentTypeRepository.save(documentType);
+		
+		return saved;
+		
+	}
 }
