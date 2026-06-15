93b3f2ef37eba8e7b8da231b9b0e6426ffbb53b4
seoh5 <dainishere3@gmail.com>
2026-06-15 14:35:06 +0900

    refactor: ddl 주석 추가 및 수정, attendance_results 테이블 workplace_id 컬럼 추가

--- database/01_ddl.sql
+++ database/01_ddl.sql
@@ -144,7 +144,7 @@ CREATE TABLE
 -- 근태관리: 근무지
 CREATE TABLE
     workplaces (
-        workplace_id NUMBER NOT NULL,
+        workplace_id NUMBER NOT NULL, -- 1 본사(정규직) 2 근무지1(일용직) 3 근무지2(일용직)
         workplace_code VARCHAR2 (30) NOT NULL,
         workplace_name VARCHAR2 (100) NOT NULL,
         workplace_address VARCHAR2 (255) NULL,
@@ -189,15 +189,16 @@ CREATE TABLE
         correction_reason_type_id NUMBER NULL, 
         employee_id NUMBER NOT NULL, 
         work_date DATE NOT NULL, -- 근무 기준 날짜
+        workplace_id NUMBER NULL, -- 근무지
         check_in_time TIMESTAMP NULL, -- 출근 시간
         check_out_time TIMESTAMP NULL, -- 퇴근 시간
-        total_work_minutes NUMBER NULL, -- 총 근무 시간(분)
-        actual_work_minutes NUMBER NULL, -- 휴게 제외 실 근무 시간(분)
+        total_work_minutes NUMBER NULL, -- 총 출근 시간(휴게 포함)
+        actual_work_minutes NUMBER NULL, -- 기본 근무 시간(휴게, 초과 근무 제외)
         overtime_minutes NUMBER NULL, -- 초과 근무 시간(분)
         is_holiday_work CHAR(1) DEFAULT 'N' NOT NULL, -- 휴일 근무 여부(Y/N)
         is_missing_checkout CHAR(1) DEFAULT 'N' NOT NULL, -- 미퇴근 여부(Y/N)
         is_correction_required CHAR(1) DEFAULT 'N' NOT NULL, -- 정정 필요 여부(Y/N)
-        processing_status VARCHAR2 (30) DEFAULT 'NORMAL' NOT NULL, -- 처리 상태(NORMAL/CORRECTION_REQUIRED/CORRECTED)
+        processing_status VARCHAR2 (30) DEFAULT 'NORMAL' NOT NULL, -- 처리 상태(NORMAL/CORRECTION_REQUIRED/CORRECTED) (미퇴근) 정상/정정 필요/정정 완료
         correction_reason VARCHAR2 (255) NULL, -- 근태 정정 사유
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
         updated_at TIMESTAMP NULL,
