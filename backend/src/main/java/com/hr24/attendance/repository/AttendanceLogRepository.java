b6248df2292dc4eb818260f8375920c794d0dbf8
seoh5 <dainishere3@gmail.com>
2026-06-16 13:05:09 +0900

    feature: 퇴근 버튼 구현 및 API 테스트 완료

--- backend/src/main/java/com/hr24/attendance/repository/AttendanceLogRepository.java
+++ backend/src/main/java/com/hr24/attendance/repository/AttendanceLogRepository.java
@@ -1,9 +1,13 @@
 package com.hr24.attendance.repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hr24.attendance.entity.AttendanceLog;
 
 public interface AttendanceLogRepository extends JpaRepository<AttendanceLog, Long>{
-	
+	// Log에 Out(퇴근) 있는지 확인
+		Optional<AttendanceLog> findByEmployeeIdAndWorkDateAndLogType(Long employeeId, LocalDate workDate, String logType);
 }
