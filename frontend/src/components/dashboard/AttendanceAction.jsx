5f236883a9dc5694896b9a88d8c23117fb14414c
seoh5 <dainishere3@gmail.com>
2026-06-18 17:50:57 +0900

    feat: 출퇴근 버튼 로직 분리 및 미출근 안내 문구 추가

--- frontend/src/components/dashboard/AttendanceAction.jsx
+++ frontend/src/components/dashboard/AttendanceAction.jsx
@@ -9,21 +9,25 @@ export default function AttendanceAction() {
     // (출근/퇴근) 완료: 0시 0분
     const handleAttendance = (type) => {
         const now = new Date();
+        if (type === "퇴근" && attendanceTime === ""){
+          alert("출근 기록이 존재하지 않습니다.");
+          return;
+        }
         const timeString = `${now.getHours()}시 ${now.getMinutes()}분`;
         setAttendanceTime(`${type} 완료: ${timeString}`);
     }
   
   return (
     <div className="p-4 bg-white border border-slate-200 rounded-xl">
-      <p className="mb-2">{attendanceTime}</p>
+      <p className="mb-2">{attendanceTime ? attendanceTime : "아직 출근 버튼을 누르지 않았습니다."}</p>
       <button 
-        onClick={handleAttendance}
+        onClick={() => handleAttendance("출근")}
         className="px-10 py-2 bg-blue-500 text-white rounded-xl"
       >
         출근
       </button>
       <button 
-        onClick={handleAttendance}
+        onClick={() => handleAttendance("퇴근")}
         className="px-10 py-2 bg-blue-500 text-white rounded-xl ml-2"
       >
         퇴근
