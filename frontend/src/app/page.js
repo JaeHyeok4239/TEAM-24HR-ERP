096933b1851590b023c32fec513baed8fef07ede
seoh5 <dainishere3@gmail.com>
2026-07-01 13:07:44 +0900

    refactor: 주석 수정(지도 영역 > 지도 및 출퇴근)

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,9 +5,13 @@ import ScheduleMiniCalendar from "@/components/dashboard/ScheduleMiniCalendar";
 export default function MainPage() {
   return (
     <div className="p-4 flex flex-col gap-4 h-full">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 h-full">
-        <div className="lg:col-span-2 bg-white rounded-xl border border-slate-200 p-4">
+      {/* 상단 2열 */}
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-4 h-1/2">
+        <div className="bg-white rounded-xl border border-slate-200 p-4 flex flex-col gap-3">
+        {/* 지도 및 출퇴근 */}
+        <div className="w-full h-75">
           <AttendancePage />
+        </div>
         </div>
         <div className="bg-white rounded-xl border border-slate-200 p-4">
           <ScheduleMiniCalendar />
