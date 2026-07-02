92b50d1bf6800328569036c919a2d8cb09ea578c
GoneAreTheDays <jhk9794@naver.com>
2026-07-02 19:33:38 +0900

    대시보드 부서별 인건비 월별로 조회 가능하도록 수정

--- backend/src/main/java/com/hr24/payroll/controller/DashboardController.java
+++ backend/src/main/java/com/hr24/payroll/controller/DashboardController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hr24.payroll.dto.DepartmentCostDto;
@@ -28,8 +29,9 @@ public class DashboardController {
     
     
     @GetMapping("/department-cost")
-    public ResponseEntity<List<DepartmentCostDto>> getDepartmentCost() {
-
-        return ResponseEntity.ok(dashboardService.getDepartmentCost());
+    public ResponseEntity<List<DepartmentCostDto>> getDepartmentCost(
+    		@RequestParam("month") String month
+    		) {
+        return ResponseEntity.ok(dashboardService.getDepartmentCost(month));
     }
 }
